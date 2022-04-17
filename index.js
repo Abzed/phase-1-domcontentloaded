2674c779944afd8a81b6112e4da59c3f40355a13
Abzed <ibraabzed@gmail.com>
2022-04-17 12:07:57 +0300

    commit tree 8bcd45cf7b357c23b1a6d9a48af5aa97e2eac497

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const p = document.querySelector('p')
+    p.textContent = 'This is really cool!'
+})
+
+
+
+
+
+
+
+
+
+
+
+
